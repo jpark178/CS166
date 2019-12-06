ba0a4feed468d33d5ac354226f905cd00e46bf09
jpark178 <jpark178@ucr.edu>
2019-12-06 02:46:36 -0800

    finished for today

--- java/DBProject.java
+++ java/DBProject.java
@@ -21,7 +21,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 /**
