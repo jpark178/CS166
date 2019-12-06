0769d200e1dc57a43d815a991984b5d75dba5958
jpark178 <jpark178@ucr.edu>
2019-12-05 21:30:26 -0800

    numberOfAvailableRooms

--- java/DBProject.java
+++ java/DBProject.java
@@ -940,6 +940,7 @@ public class DBProject {
    			}catch(Exception e) {
    				System.out.println("Query was not executed.");
    			}
+   		}
       // ...
       // ...
    }//end numberOfAvailableRooms
