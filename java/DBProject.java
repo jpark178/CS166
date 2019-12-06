5079693de9654013ee6b6bea81e8efd93171b3a0
jpark178 <jpark178@ucr.edu>
2019-12-06 01:37:06 -0800

    test

--- java/DBProject.java
+++ java/DBProject.java
@@ -22,7 +22,6 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Date;
-import java.
 /**
  * This class defines a simple embedded SQL utility class that is designed to
  * work with PostgreSQL JDBC drivers.
