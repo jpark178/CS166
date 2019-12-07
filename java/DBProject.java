b15231ef45c4ea9f0efd798d3fd50fd28cb08079
jpark178 <jpark178@ucr.edu>
2019-12-06 20:10:02 -0800

    finish

--- java/DBProject.java
+++ java/DBProject.java
@@ -1477,7 +1477,7 @@ public class DBProject {
 	  
 	  String query;
 	  try {
-		  query = "SELECT * FROM Booking B WHERE B.bookingDate >= '" + startingDate + "'' AND B.bookingDate <= '" + endingDate + "'' ORDER BY B.price DESC LIMIT " + k + ";";
+		  query = "SELECT * FROM Booking B WHERE B.bookingDate >= \'" + startingDate + "\' AND B.bookingDate <= \'" + endingDate + "\'' ORDER BY B.price DESC LIMIT " + k + ";";
 		  esql.executeQuery(query);
 	  } catch (Exception e) {
 		  System.out.println("Query failed. " + e.getMessage());
