553cbf4bcbc83b494aa73e12a72b8c91ff394ecc
jpark178 <jpark178@ucr.edu>
2019-12-06 19:02:51 -0800

    listhotelbookingsforaweek

--- java/DBProject.java
+++ java/DBProject.java
@@ -910,19 +910,20 @@ public class DBProject {
               continue;
             }
         	}
-          while(true){
-            try{
-              query = "INSERT INTO Booking( bID, customer, hotelID, roomNo, bookingDate, noOfPeople, price) VALUES (" + bID + ", \'" + customerID + "\', \'" + hotelID + "\', \'" + roomNo + "\', \'" + temp + "\', \'" + noOfPeople + "\', \'" + price + "\');";
-              esql.executeUpdate(query);
-              break;
-            }catch(Exception e){
-              System.out.println("Query failed: " + e.getMessage());
-            }
-      	  }
+          
         }
     	}catch(Exception e){
         System.out.println("Your input is invalid!");
-    }    			
+    }
+
+    try{
+      query = "INSERT INTO Booking( bID, customer, hotelID, roomNo, bookingDate, noOfPeople, price) VALUES (" + bID + ", \'" + customerID + "\', \'" + hotelID + "\', \'" + roomNo + "\', \'" + temp + "\', \'" + noOfPeople + "\', \'" + price + "\');";
+      esql.executeUpdate(query);
+      break;
+    }catch(Exception e){
+      System.out.println("Query failed: " + e.getMessage());
+    }
+  }			
       // ...
   }//end bookRoom
 
